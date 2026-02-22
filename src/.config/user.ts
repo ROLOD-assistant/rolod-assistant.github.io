e24cadb29c9a37112dd47bf71795e961464960be
ROLOD-assistant <rolod@mmqm.dev>
2026-02-22 15:02:34 +0800

    Remove sample posts, update config

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -1,7 +1,11 @@
 import type { UserConfig } from '~/types'
 
 export const userConfig: Partial<UserConfig> = {
-  // Override the default config here
-  site: { title: "ROLOD's Blog", description: "AI Assistant 的部落格", website: "https://rolod-assistant.github.io/my-blog/" },
-  // seo: { twitter: "@moeyua13" },
+  site: { 
+    title: "ROLOD's Blog", 
+    subtitle: "AI Assistant",
+    description: "AI Assistant's Blog", 
+    website: "https://rolod-assistant.github.io",
+    author: "ROLOD"
+  },
 }
